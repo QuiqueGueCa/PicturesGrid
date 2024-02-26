4a70a1c5971193a764f8dd72651b50cd4b5dd94f
QuiqueGueCa <enrique.guevara@vasscompany.com>
2024-02-26 12:18:27 +0100

    added options to menuAdd

--- app/src/main/java/com/example/picturesgrid/ui/grid_fragment/GridFragment.kt
+++ app/src/main/java/com/example/picturesgrid/ui/grid_fragment/GridFragment.kt
@@ -4,18 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.picturesgrid.R
 import com.example.picturesgrid.data.Picture
 import com.example.picturesgrid.databinding.FragmentGridBinding
 import com.example.picturesgrid.ui.grid_fragment.adapter.PicturesAdapter
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class GridFragment : Fragment(), PicturesAdapter.PictureListener {
 
     private lateinit var binding: FragmentGridBinding
-    private lateinit var mAdaper: PicturesAdapter
+    private lateinit var mAdapter: PicturesAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -30,20 +30,29 @@ class GridFragment : Fragment(), PicturesAdapter.PictureListener {
 
         setupToolBar()
         setupAdapter()
-
     }
 
     private fun setupAdapter() {
-        mAdaper = PicturesAdapter(mutableListOf(), this)
+        mAdapter = PicturesAdapter(mutableListOf(), this)
         binding.recyclerView.setHasFixedSize(true)
         binding.recyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
-        binding.recyclerView.adapter = mAdaper
+        binding.recyclerView.adapter = mAdapter
     }
 
     private fun setupToolBar() {
         binding.toolBar.inflateMenu(R.menu.menu_add)
         binding.toolBar.menu.getItem(0).setOnMenuItemClickListener {
-            Toast.makeText(requireContext(), "asdadsasds", Toast.LENGTH_SHORT).show()
+
+            val items = resources.getStringArray(R.array.array_options_item)
+
+            MaterialAlertDialogBuilder(requireContext())
+                .setTitle(R.string.dialog_options_title)
+                .setItems(items) { _, which ->
+                    when (which) {
+                        0 -> {} // TODO: decírselo al viewModel
+                        1 -> {} // TODO: decírselo al viewModel
+                    }
+                }.show()
             true
         }
     }
